74583e458e4355e2d5f9ac780247ec743ac3b057
Fi-Boo <s2008156@student.rmit.edu.au>
2024-04-14 21:17:46 +1000

    ccloudA2

---
+++ src/main/java/com/test/musicrecorder/MusicrecorderApplication.java
@@ -0,0 +1,13 @@
+package com.test.musicrecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicrecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicrecorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/musicrecorder/MusicrecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.musicrecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicrecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
