7f31c14dad5cb1a55b0ee7a9ab260161dd0da411
Fi-Boo <s2008156@student.rmit.edu.au>
2024-04-20 10:19:29 +1000

    start of day

--- src/main/java/com/cca2/musiclibrary/MusiclibraryApplication.java
+++ src/main/java/com/cca2/musiclibrary/MusiclibraryApplication.java
@@ -2,12 +2,24 @@ package com.cca2.musiclibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @SpringBootApplication
 public class MusiclibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusiclibraryApplication.class, args);
+	}
+
+	@Bean
+	public DynamoDbClient dynamoDBClient() {
+		Region region = Region.US_EAST_1;
+		DynamoDbClient ddb = DynamoDbClient.builder().region(region).build();
+
+		return ddb;
 	}
 
 }
